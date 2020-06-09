3ecd615db4bb3acf1c54e52f61652a83dffbe1da
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-06-09 21:30:31 +0300

    Add Taco model

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/model/Taco.java
@@ -0,0 +1,11 @@
+package by.kiselevich.tacocloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
