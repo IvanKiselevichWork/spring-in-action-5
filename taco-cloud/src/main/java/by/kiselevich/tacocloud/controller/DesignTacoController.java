b18c10cf9d957f1ad31e26a2f0b950231930a3b6
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-06-11 20:36:46 +0300

    Add validation errors handling to Taco POST prosessing

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/controller/DesignTacoController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,39 +14,51 @@ import by.kiselevich.tacocloud.model.Taco;
 import by.kiselevich.tacocloud.model.Ingredient;
 import by.kiselevich.tacocloud.model.Ingredient.Type;
 
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    private static final List<Ingredient> INGREDIENTS = Arrays.asList(
+            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+            new Ingredient("CHED", "Cheddar", Type.CHEESE),
+            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+            new Ingredient("SLSA", "Salsa", Type.SAUCE),
+            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+    );
+
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-        Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-        }
-        model.addAttribute("design", new Taco());
+        addIngredientsToModel(model);
+        model.addAttribute("taco", new Taco());
         return "design";
     }
 
     @PostMapping
-    public String processDesign(Design design) {
+    public String processDesign(@Valid Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            addIngredientsToModel(model);
+            return "design";
+        }
+
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
+    }
+
+    private void addIngredientsToModel(Model model) {
+        Type[] types = Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(INGREDIENTS, type));
+        }
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
