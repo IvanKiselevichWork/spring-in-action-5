b700a88aa2a167d1aa374ea44993a35c96d0a584
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-03 21:12:05 +0300

    Setup SecurityConfig with new UserDetailsService

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/security/SecurityConfig.java
@@ -1,29 +1,30 @@
 package by.kiselevich.tacocloud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    DataSource dataSource;
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "select username, password, enabled from Users " + "where username=?")
-                .authoritiesByUsernameQuery(
-                        "select username, authority from User_Authorities " + "where username=?")
-                .passwordEncoder(new BCryptPasswordEncoder());
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
