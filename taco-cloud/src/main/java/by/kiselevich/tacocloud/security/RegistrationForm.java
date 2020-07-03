563ae7b53abfbfe6e8cb7b1adc1cb19b37a6ba11
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-03 21:23:50 +0300

    Add registration form utility class

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/security/RegistrationForm.java
@@ -0,0 +1,23 @@
+package by.kiselevich.tacocloud.security;
+
+import by.kiselevich.tacocloud.model.User;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Data
+public class RegistrationForm {
+
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return new User(username, passwordEncoder.encode(password),
+                fullname, street, city, state, zip, phoneNumber);
+    }
+}
