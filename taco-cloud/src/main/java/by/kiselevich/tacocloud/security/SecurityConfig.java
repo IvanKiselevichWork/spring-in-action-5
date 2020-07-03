ecd6d64cd0023ae29581f4e9c393addf309813e5
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-03 15:05:20 +0300

    Add in-memory user store

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package by.kiselevich.tacocloud.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("1").password("{noop}1").roles("USER")
+                .and()
+                .withUser("2").password("{noop}2").authorities("USER", "ADMIN");
+    }
+}
