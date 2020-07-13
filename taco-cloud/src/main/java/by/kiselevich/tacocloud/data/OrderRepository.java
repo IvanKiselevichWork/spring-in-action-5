84cb150445b7c7b1ae80944546a2b9039e613e8a
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-13 09:21:02 +0300

    Add method to OrderRepository findByUserOrderByPlacedAtDesc

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/data/OrderRepository.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/data/OrderRepository.java
@@ -1,6 +1,8 @@
 package by.kiselevich.tacocloud.data;
 
 import by.kiselevich.tacocloud.model.Order;
+import by.kiselevich.tacocloud.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.sql.Timestamp;
@@ -11,4 +13,6 @@ public interface OrderRepository extends CrudRepository<Order, String> {
     List<Order> findByZip(String deliveryZip);
 
     List<Order> readOrdersByZipAndPlacedAtBetween(String zip, Timestamp startDate, Timestamp endDate);
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
