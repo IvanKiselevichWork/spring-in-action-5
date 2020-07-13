80998c99e430d36d7a34b6bfc25347557441b8da
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-13 11:33:58 +0300

    Delete repositories created with JdbcTemplate

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package by.kiselevich.tacocloud.repository;
-
-import by.kiselevich.tacocloud.model.Order;
-
-public interface OrderRepository {
-    Order save(Order order);
-}
--- taco-cloud/src/main/java/by/kiselevich/tacocloud/repository/TacoRepository.java
+++
@@ -1,7 +0,0 @@
-package by.kiselevich.tacocloud.repository;
-
-import by.kiselevich.tacocloud.model.Taco;
-
-public interface TacoRepository {
-    Taco save(Taco taco);
-}
