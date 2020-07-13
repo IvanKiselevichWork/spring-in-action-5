724958e238fa622e04b6d98ca111f78d44fa0a96
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-13 09:20:07 +0300

    Add class for store Order properties

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/props/OrderProps.java
@@ -0,0 +1,12 @@
+package by.kiselevich.tacocloud.props;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "taco.order")
+@Data
+public class OrderProps {
+    private int pageSize = 20;
+}
