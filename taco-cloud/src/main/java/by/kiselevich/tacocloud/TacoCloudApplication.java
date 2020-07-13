e459e69d8c7ccea088cff0568ffa6d5b7fdd82f5
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-13 10:22:01 +0300

    Add metadata for taco.order.pageSize property

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/TacoCloudApplication.java
@@ -1,10 +1,13 @@
 package by.kiselevich.tacocloud;
 
+import by.kiselevich.tacocloud.props.OrderProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OrderProps.class)
 public class TacoCloudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
