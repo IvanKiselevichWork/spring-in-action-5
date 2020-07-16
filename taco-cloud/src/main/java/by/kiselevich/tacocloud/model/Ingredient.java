d218e4716374ea7b431c0e2198cf7a19b7d05fce
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-16 16:17:19 +0300

    Make Ingredient extends RepresentationModel

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/model/Ingredient.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/model/Ingredient.java
@@ -1,18 +1,16 @@
 package by.kiselevich.tacocloud.model;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Data
+@Getter
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @Entity
-public class Ingredient {
+public class Ingredient extends RepresentationModel<Ingredient> {
     @Id
     private final String id;
     private final String name;
