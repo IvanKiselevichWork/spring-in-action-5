81a6de82e6e496cb5ce0c22ca658918f9d770976
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-16 16:20:32 +0300

    Replace List<Ingredient> with CollectionModel<IngredientResource> in TacoResource

--- taco-cloud/src/main/java/by/kiselevich/tacocloud/model/TacoResource.java
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/model/TacoResource.java
@@ -1,23 +1,26 @@
 package by.kiselevich.tacocloud.model;
 
+import by.kiselevich.tacocloud.resourceassembler.IngredientResourceAssembler;
 import lombok.Getter;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 public class TacoResource extends RepresentationModel<Taco> {
+
+    private static final IngredientResourceAssembler INGREDIENT_RESOURCE_ASSEMBLER = new IngredientResourceAssembler();
 
     @Getter
     private final String name;
     @Getter
     private final Timestamp createdAt;
     @Getter
-    private final List<Ingredient> ingredients;
+    private final CollectionModel<IngredientResource> ingredients;
 
     public TacoResource(Taco taco) {
         this.name = taco.getName();
         this.createdAt = taco.getCreatedAt();
-        this.ingredients = taco.getIngredients();
+        this.ingredients = INGREDIENT_RESOURCE_ASSEMBLER.toCollectionModel(taco.getIngredients());
     }
 }
