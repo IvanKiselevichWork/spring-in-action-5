c98268e8d1821de59bce54f3b22c6ba578515a3f
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-23 14:27:53 +0300

    Set up receiving an already-converted Order object

--- taco-cloud/src/main/java/kitchen/messaging/jms/JmsOrderReceiver.java
+++ taco-cloud/src/main/java/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -5,32 +5,28 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Destination;
-import javax.jms.Message;
 
 @Component
 @Slf4j
 public class JmsOrderReceiver implements OrderReceiver {
 
     private final JmsTemplate jms;
-    private final MessageConverter converter;
     private final Destination orderQueue;
 
     @Autowired
-    public JmsOrderReceiver(JmsTemplate jms, MessageConverter converter, Destination orderQueue) {
+    public JmsOrderReceiver(JmsTemplate jms, Destination orderQueue) {
         this.jms = jms;
-        this.converter = converter;
         this.orderQueue = orderQueue;
     }
 
     @Override
     @SneakyThrows
     public Order receiveOrder() {
-        Message message = jms.receive(orderQueue);
-        log.debug("Received message: {}", message);
-        return (Order) converter.fromMessage(message);
+        Order order = (Order) jms.receiveAndConvert(orderQueue);
+        log.debug("Received message: {}", order);
+        return order;
     }
 }
