3518333e3e98d67d164b6e2a6659069ee04d3441
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-24 10:11:17 +0300

    Add OrderMessagingService implementation with RabbitMQ

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/service/RabbitOrderMessagingService.java
@@ -0,0 +1,28 @@
+package by.kiselevich.tacocloud.service;
+
+import by.kiselevich.tacocloud.model.Order;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitOrderMessagingService implements OrderMessagingService {
+
+    private final RabbitTemplate rabbit;
+
+    @Autowired
+    public RabbitOrderMessagingService(RabbitTemplate rabbit) {
+        this.rabbit = rabbit;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        MessageConverter converter = rabbit.getMessageConverter();
+        MessageProperties props = new MessageProperties();
+        Message message = converter.toMessage(order, props);
+        rabbit.send("tacocloud.order", message);
+    }
+}
