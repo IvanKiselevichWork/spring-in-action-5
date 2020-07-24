45ac6b7af61a23597a435572b2504aa71bddb842
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-24 11:40:05 +0300

    Add inner class configuration for XML file configuration with xmlconfig profile

---
+++ simple-flow/src/main/java/by/kiselevich/simpeflow/FileWriterIntegrationConfig.java
@@ -0,0 +1,14 @@
+package by.kiselevich.simpeflow;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Profile("xmlconfig")
+    @Configuration
+    @ImportResource("classpath:/filewriter-config.xml")
+    public static class XmlConfiguration {}
+}
