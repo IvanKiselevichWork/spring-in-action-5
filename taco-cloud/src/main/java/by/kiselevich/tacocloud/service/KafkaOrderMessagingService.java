43051c8761b74665ceffca096ce63660ffd721a5
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-24 10:38:18 +0300

    Add OrderMessagingService implementation with Kafka

---
+++ taco-cloud/src/main/java/by/kiselevich/tacocloud/service/KafkaOrderMessagingService.java
@@ -0,0 +1,22 @@
+package by.kiselevich.tacocloud.service;
+
+import by.kiselevich.tacocloud.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaOrderMessagingService implements OrderMessagingService {
+
+    private final KafkaTemplate<String, Order> kafkaTemplate;
+
+    @Autowired
+    public KafkaOrderMessagingService(KafkaTemplate<String, Order> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        kafkaTemplate.sendDefault(order);
+    }
+}
