60ec03cb59ece227b7d2e8132cdad743e3ddd56d
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:13:31 +0300

    Add creating Flux data from interval

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -76,6 +77,18 @@ public class FluxCreateTest {
                 .expectNext(3)
                 .expectNext(4)
                 .expectNext(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_interval() {
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofMillis(100)).take(5);
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
                 .verifyComplete();
     }
 }
