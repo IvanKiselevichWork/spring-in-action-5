3bd1248a4e869593a93d1709fbef92fbe5fc04f8
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:29:34 +0300

    Add Flux skip test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -1,4 +1,18 @@
 package by.kiselevich.reactor;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 public class FluxTransformTest {
+
+    @Test
+    public void skipAFew() {
+        Flux<String> skipFlux = Flux
+                .just("one", "two", "skip a few", "ninety nine", "one hundred")
+                .skip(3);
+        StepVerifier.create(skipFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+    }
 }
