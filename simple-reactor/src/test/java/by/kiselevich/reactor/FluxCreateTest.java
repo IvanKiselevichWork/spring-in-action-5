68693e09c61bcb9e1c2231af2b43235f14d4bbc5
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:01:10 +0300

    Add creating Flux data from array

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
@@ -18,4 +18,17 @@ public class FluxCreateTest {
                 .expectNext("Strawberry")
                 .verifyComplete();
     }
+
+    @Test
+    public void createAFlux_fromArray() {
+        String[] fruits = new String[] {"Apple", "Orange", "Grape", "Banana", "Strawberry" };
+        Flux<String> fruitFlux = Flux.fromArray(fruits);
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
 }
