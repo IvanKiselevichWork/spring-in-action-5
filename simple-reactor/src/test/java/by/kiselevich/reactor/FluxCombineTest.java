f7628f34aee1d00b1964ce75191427c399493938
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:27:26 +0300

    Add Flux first from two sources test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxCombineTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCombineTest.java
@@ -64,4 +64,17 @@ public class FluxCombineTest {
                 .expectNext("Barbossa eats Apples")
                 .verifyComplete();
     }
+
+    @Test
+    public void firstFlux() {
+        Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+        Flux<String> firstFlux = Flux.first(slowFlux, fastFlux);
+        StepVerifier.create(firstFlux)
+                .expectNext("hare")
+                .expectNext("cheetah")
+                .expectNext("squirrel")
+                .verifyComplete();
+    }
 }
