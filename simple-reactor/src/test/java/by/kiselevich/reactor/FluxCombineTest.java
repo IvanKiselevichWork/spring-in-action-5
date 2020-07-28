7f7baf6b45e3c96e7ee5821f2d58538cdfebf323
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:20:59 +0300

    Add Flux zip to one object test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxCombineTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCombineTest.java
@@ -49,4 +49,19 @@ public class FluxCombineTest {
                                 p.getT2().equals("Apples"))
                 .verifyComplete();
     }
+
+    @Test
+    public void zipFluxesToObject() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+        Flux<String> zippedFlux =
+                Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+        StepVerifier.create(zippedFlux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
 }
