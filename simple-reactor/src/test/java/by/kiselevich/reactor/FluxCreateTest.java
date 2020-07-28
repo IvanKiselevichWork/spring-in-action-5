0fd1208d900c03ed5f264a39bafbc0a845b00609
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:03:43 +0300

    Add creating Flux data from range

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCreateTest.java
@@ -66,4 +66,16 @@ public class FluxCreateTest {
                 .expectNext("Strawberry")
                 .verifyComplete();
     }
+
+    @Test
+    public void createAFlux_range() {
+        Flux<Integer> intervalFlux = Flux.range(1, 5);
+        StepVerifier.create(intervalFlux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectNext(5)
+                .verifyComplete();
+    }
 }
