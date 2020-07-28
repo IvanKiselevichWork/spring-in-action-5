7b97aa176e10d87e39d6899f24d3728dd8d9fa91
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 10:16:51 +0300

    Add FluxCombineTest with Flux merge test

---
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxCombineTest.java
@@ -0,0 +1,30 @@
+package by.kiselevich.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxCombineTest {
+
+    @Test
+    public void mergeFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+        StepVerifier.create(mergedFlux)
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+}
