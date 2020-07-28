4f830bd67f581db6d0a00b61092e032bd0594974
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 11:22:16 +0300

    Add Flux take test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -28,4 +28,14 @@ public class FluxTransformTest {
                 .expectNext("ninety nine", "one hundred")
                 .verifyComplete();
     }
+
+    @Test
+    public void take() {
+        Flux<String> nationalParkFlux = Flux
+                .just("Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                .take(3);
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+                .verifyComplete();
+    }
 }
