ab8fea33c67a5a16ff6b1096cdf24c24c87a5dea
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 11:20:57 +0300

    Add Flux skip with duration test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 public class FluxTransformTest {
 
     @Test
@@ -11,6 +13,17 @@ public class FluxTransformTest {
         Flux<String> skipFlux = Flux
                 .just("one", "two", "skip a few", "ninety nine", "one hundred")
                 .skip(3);
+        StepVerifier.create(skipFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+    }
+
+    @Test
+    public void skipAFewSeconds() {
+        Flux<String> skipFlux = Flux
+                .just("one", "two", "skip a few", "ninety nine", "one hundred")
+                .delayElements(Duration.ofMillis(100))
+                .skip(Duration.ofMillis(400));
         StepVerifier.create(skipFlux)
                 .expectNext("ninety nine", "one hundred")
                 .verifyComplete();
