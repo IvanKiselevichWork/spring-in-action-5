8189de0fa01eaa50cb3ee2bf9d49ea33fd834d6a
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-28 11:26:06 +0300

    Add Flux filter test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -49,4 +49,14 @@ public class FluxTransformTest {
                 .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
                 .verifyComplete();
     }
+
+    @Test
+    public void filter() {
+        Flux<String> nationalParkFlux = Flux
+                .just("Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                .filter(np -> !np.contains(" "));
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Zion")
+                .verifyComplete();
+    }
 }
