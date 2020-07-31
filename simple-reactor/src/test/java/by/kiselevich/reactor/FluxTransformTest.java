993052d708dec176d7a18585ef3fbe3ee8ce4491
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-31 10:43:29 +0300

    Add Flux flatMap test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -2,9 +2,13 @@ package by.kiselevich.reactor;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class FluxTransformTest {
 
@@ -82,6 +86,28 @@ public class FluxTransformTest {
                 .expectNext(new Player("Michael", "Jordan"))
                 .expectNext(new Player("Scottie", "Pippen"))
                 .expectNext(new Player("Steve", "Kerr"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void flatMap() {
+        Flux<Player> playerFlux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                .flatMap(n -> Mono.just(n)
+                        .map(p -> {
+                            String[] split = p.split("\\s");
+                            return new Player(split[0], split[1]);
+                        })
+                        .subscribeOn(Schedulers.parallel())
+                );
+        List<Player> playerList = Arrays.asList(
+                new Player("Michael", "Jordan"),
+                new Player("Scottie", "Pippen"),
+                new Player("Steve", "Kerr"));
+        StepVerifier.create(playerFlux)
+                .expectNextMatches(playerList::contains)
+                .expectNextMatches(playerList::contains)
+                .expectNextMatches(playerList::contains)
                 .verifyComplete();
     }
 }
