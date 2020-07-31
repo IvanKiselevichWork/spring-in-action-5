e2a5ef0a3cb4ea8b74e41f4cbf38c3fe766645cd
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-07-31 10:38:40 +0300

    Add Flux map test

--- simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
+++ simple-reactor/src/test/java/by/kiselevich/reactor/FluxTransformTest.java
@@ -69,4 +69,19 @@ public class FluxTransformTest {
                 .expectNext("dog", "cat", "bird", "anteater")
                 .verifyComplete();
     }
+
+    @Test
+    public void map() {
+        Flux<Player> playerFlux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                .map(n -> {
+                    String[] split = n.split("\\s");
+                    return new Player(split[0], split[1]);
+                });
+        StepVerifier.create(playerFlux)
+                .expectNext(new Player("Michael", "Jordan"))
+                .expectNext(new Player("Scottie", "Pippen"))
+                .expectNext(new Player("Steve", "Kerr"))
+                .verifyComplete();
+    }
 }
