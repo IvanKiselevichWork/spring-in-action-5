cdd379dae5c6399d7187d78b6302461abc4a33bf
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-08-24 11:06:52 +0300

    Add NoArgsConstructor to Taco

--- reactive-taco-cloud/src/main/java/by/kiselevich/model/Taco.java
+++ reactive-taco-cloud/src/main/java/by/kiselevich/model/Taco.java
@@ -2,12 +2,14 @@ package by.kiselevich.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Taco {
     private Long id;
     private Timestamp createdAt;
